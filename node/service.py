37f9e876d43f8f37e2ed4a4569d6fb283a552684
Ruzal <email.com>
2023-11-03 12:50:11 +0300

    Second task

--- node/service.py
+++ node/service.py
@@ -10,7 +10,7 @@ from uc_http_requester.requester import Request
 
 
 class NodeType(flow.NodeType):
-    id: str = 'GoogleExtension'
+    id: str = 'fe19a64a-c274-412d-a6e8-66b9639e1f3a'
     type: flow.NodeType.Type = flow.NodeType.Type.action
     name: str = 'GoogleExtension'
     displayName: str = 'GoogleExtension'
@@ -18,14 +18,33 @@ class NodeType(flow.NodeType):
     description: str = 'GoogleExtension'
     properties: List[Property] = [
         Property(
-            displayName='Тестовое поле',
-            name='foo_field',
-            type=Property.Type.JSON,
-            placeholder='Foo placeholder',
-            description='Foo description',
+            displayName='Текстовое поле',
+            name='text_field',
+            type=Property.Type.STRING,
+            placeholder='Введите текст',
+            description='Текстовое поле для ввода данных',
             required=True,
             default='Test data',
-        )
+        ),
+        Property(
+            displayName='Числовое поле',
+            name='number_field',
+            type=Property.Type.NUMBER,
+            placeholder='Введите число',
+            description='Числовое поле для ввода данных',
+            required=True,
+            default=0,
+        ),
+        Property(
+            displayName='Переключатель',
+            name='field_type',
+            type=Property.Type.BOOLEAN,
+            placeholder='Выберите тип поля',
+            description='Выберите тип поля: если включено, текстовое поле '
+            'будет обработано как число; если выключено — как текст.',
+            required=True,
+            default=False,
+        ),
     ]
 
 
@@ -37,9 +56,22 @@ class InfoView(info.Info):
 class ExecuteView(execute.Execute):
     async def post(self, json: NodeRunContext) -> NodeRunContext:
         try:
-            await json.save_result({
-                "result": json.node.data.properties['foo_field']
-            })
+            text_field = json.node.data.properties.get('text_field', '')
+            number_field = json.node.data.properties.get('number_field', 0)
+            field_type = json.node.data.properties.get('field_type', False)
+            result = None
+
+            if field_type:
+                try:
+                    text_field_as_number = int(text_field)
+                    result = text_field_as_number + number_field
+                except ValueError:
+                    raise ValueError(
+                        'Невозможно преобразовать текстовое поле в число.'
+                    )
+            else:
+                result = text_field + str(number_field)
+            await json.save_result({"result": result})
             json.state = RunState.complete
         except Exception as e:
             self.log.warning(f'Error {e}')
