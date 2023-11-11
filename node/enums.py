01d1be5d444be7ca9db798b1466cea958848d648
Ruzal <email.com>
2023-11-11 15:32:16 +0000

    Working version of task 3

---
+++ node/enums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class SwitchValues(str, Enum):
+    value1 = 'Значение 1'
+    value2 = 'Значение 2'
--- node/service.py
+++ node/service.py
@@ -1,13 +1,12 @@
 import ujson
-from typing import List, Tuple
+from typing import List
 
 from uc_flow_nodes.schemas import NodeRunContext
 from uc_flow_nodes.service import NodeService
 from uc_flow_nodes.views import info, execute
 from uc_flow_schemas import flow
-from uc_flow_schemas.flow import Property, CredentialProtocol, RunState
-from uc_http_requester.requester import Request
-
+from uc_flow_schemas.flow import Property, RunState, OptionValue
+from node.enums import SwitchValues 
 
 class NodeType(flow.NodeType):
     id: str = 'fe19a64a-c274-412d-a6e8-66b9639e1f3a'
@@ -18,65 +17,98 @@ class NodeType(flow.NodeType):
     description: str = 'GoogleExtension'
     properties: List[Property] = [
         Property(
-            displayName='Текстовое поле',
-            name='text_field',
-            type=Property.Type.STRING,
-            placeholder='Введите текст',
-            description='Текстовое поле для ввода данных',
-            required=True,
-            default='Test data',
-        ),
-        Property(
-            displayName='Числовое поле',
-            name='number_field',
-            type=Property.Type.NUMBER,
-            placeholder='Введите число',
-            description='Числовое поле для ввода данных',
-            required=True,
-            default=0,
-        ),
-        Property(
             displayName='Переключатель',
-            name='field_type',
+            name='switch_field',
             type=Property.Type.BOOLEAN,
-            placeholder='Выберите тип поля',
-            description='Выберите тип поля: если включено, текстовое поле '
-            'будет обработано как число; если выключено — как текст.',
+            placeholder='Включено/Выключено',
             required=True,
             default=False,
         ),
+        Property(
+            displayName='Значение 1',
+            name='value_1',
+            type=Property.Type.OPTIONS,
+            required=False,
+            default='',
+            options=[
+                OptionValue(name='Значение 1', 
+                            value=SwitchValues.value1,
+                            description=''),
+                OptionValue(name='Значение 2', 
+                            value=SwitchValues.value2,  
+                            description=''),
+            ],
+            displayOptions={
+                "show": {"switch_field": [True]},
+                "hide": {"switch_field": [False]}
+            },
+        ),
+        Property(
+            displayName='Значение 2',
+            name='value_2',
+            type=Property.Type.OPTIONS,
+            required=False,
+            default='',
+            options=[
+                OptionValue(name='Значение 1', 
+                            value=SwitchValues.value1, 
+                            description=''),
+                OptionValue(name='Значение 2', 
+                            value=SwitchValues.value2,  
+                            description=''),
+            ],
+            displayOptions={
+                "show": {"switch_field": [True]},
+                "hide": {"switch_field": [False]}
+            },
+        ),
+        Property(
+            displayName='Поле для ввода почты',
+            name='email_input',
+            type=Property.Type.STRING,
+            required=False,
+            default='',
+            displayOptions={
+                "show": {"value_1": [SwitchValues.value1], "value_2": [SwitchValues.value1]},
+                "hide": {"value_1": [SwitchValues.value2], "value_2": [SwitchValues.value2]}
+            },
+        ),
+        Property(
+            displayName='Поле для ввода даты и времени',
+            name='datetime_input',
+            type=Property.Type.DATETIME,
+            required=False,
+            default='',
+            displayOptions={
+                "show": {"value_1": [SwitchValues.value2], "value_2": [SwitchValues.value2]},
+                "hide": {"value_1": [SwitchValues.value1], "value_2": [SwitchValues.value1]}
+            },
+        ),
     ]
-
 
 class InfoView(info.Info):
     class Response(info.Info.Response):
         node_type: NodeType
 
-
 class ExecuteView(execute.Execute):
     async def post(self, json: NodeRunContext) -> NodeRunContext:
         try:
-            text_field = json.node.data.properties.get('text_field', '')
-            number_field = json.node.data.properties.get('number_field', 0)
-            field_type = json.node.data.properties.get('field_type', False)
-            result = None
+            switch_field = json.node.data.properties.switch_field
 
-            if field_type:
-                try:
-                    text_field_as_number = int(text_field)
-                    result = text_field_as_number + number_field
-                except ValueError:
-                    raise ValueError(
-                        'Невозможно преобразовать текстовое поле в число.'
-                    )
+            if switch_field:
+                value_1 = json.node.data.properties.value_1
+                value_2 = json.node.data.properties.value_2
+
+                result = f"Переключатель включен. Значение 1: {value_1}, Значение 2: {value_2}"
+                await json.save_result({"result": result})
             else:
-                result = text_field + str(number_field)
-            await json.save_result({"result": result})
-            json.state = RunState.complete
+                await json.save_result({"result": "Переключатель выключен. Нет данных для отображения."})
+
         except Exception as e:
             self.log.warning(f'Error {e}')
             await json.save_error(str(e))
             json.state = RunState.error
+
         return json
 
 
